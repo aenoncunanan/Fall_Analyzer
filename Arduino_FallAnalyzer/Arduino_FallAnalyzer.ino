8b5fe826be3acc154fe0dbb7cb35198129addb74
aenoncunanan <aenon_cunanan2@yahoo.com>
2017-10-06 15:28:12 +0800

    Made the board to recognize wether the board is facing up, down, left, right, front, and back.

--- Arduino_FallAnalyzer/Arduino_FallAnalyzer.ino
+++ Arduino_FallAnalyzer/Arduino_FallAnalyzer.ino
@@ -3,20 +3,6 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-// Define Pins
-#define OLED_CLK  12  //D0
-#define OLED_MOSI 11  //D1
-#define OLED_RESET  10  //RES
-#define OLED_DC   9 //DC
-#define OLED_CS   8 //CS
-
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-#if (SSD1306_LCDHEIGHT != 64)
-
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
 int lastOrientation = - 1;  // Previous orientation (for comparison)
 
@@ -31,21 +17,12 @@ void setup() {
   
 // Initialize the display
   Serial.println("Initializing display...");
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
   
 // Set the accelerometer range to 2G
   CurieIMU.setAccelerometerRange(2);
   
 // Prompt a welcome message  
   Serial.println("Device is ready!"); 
-  display.setCursor(20,30);
-  display.print("Device is ready!");
-  display.setCursor(0,50);
-  display.print("Ambion Cunanan Harris");
-  display.display();
   delay(3000);
 }
 
@@ -58,17 +35,8 @@ void setup() {
   4: portrait, USB connector up
   5: portrait, USB connector down
   */
-  
-  /*
-    The orientation of the screen:
-    0: default
-    1: 90 degrees CCW
-    2: 180 CW
-    3: 90 degress CW
-  */
 
 void loop() {
-  display.clearDisplay();   //clear display
   int orientation = - 1;    // the board's orientation
   String orientationString; // string for printing description of orientation
 
@@ -87,40 +55,28 @@ void loop() {
     if (z > 0) {
       orientationString = "up";
       orientation = 0;
-      display.clearDisplay();
-      display.display();
  
     } else {
       orientationString = "down";
       orientation = 1;
-      display.clearDisplay();
-      display.display();
     }
   } else if ( (absY > absX) && (absY > absZ)) {
     // base orientation on Y
     if (y > 0) {
-      orientationString = "digital pins up / In Sitting Position!";
-      orientation = 2;
-      display.setRotation(0);
-      displaySitting();       
+      orientationString = "digital pins up";
+      orientation = 2;      
     } else {
-      orientationString = "analog pins up / In Sitting Position";
+      orientationString = "analog pins up";
       orientation = 3;  
-      display.setRotation(2);
-      displaySitting();
     }
   } else {
     // base orientation on X
     if (x < 0) {
-      orientationString = "connector up / In Standing Position";
+      orientationString = "connector up";
       orientation = 4;
-      display.setRotation(3);
-      displayStanding(); 
     } else {
-      orientationString = "connector down / In Standing Position";
-      orientation = 5;
-      display.setRotation(1);      
-      displayStanding();          
+      orientationString = "connector down";
+      orientation = 5;              
     }
   }
 
@@ -129,22 +85,4 @@ void loop() {
     Serial.println(orientationString);
     lastOrientation = orientation;
   }
-}
-
-void displaySitting() {
-  display.setCursor(35,20);
-  display.println("IN SITTING");
-  display.setCursor(40,40);
-  display.println("POSITION!");
-  display.display();
-}
-
-void displayStanding() {
-  display.setCursor(20,20);
-  display.println("IN");
-  display.setCursor(6,40);
-  display.println("STANDING");
-  display.setCursor(6,60);
-  display.println("POSITION!");
-  display.display();  
 }
